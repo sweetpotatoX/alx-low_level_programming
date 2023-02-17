58e5086b77786b4fb647856d98a27f98558bc4de
sweetpotatoX <abdobahaa53@gmail.com>
2023-02-17 05:08:37 -0800

    masd

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
-*main - prints the alphabet in lowercase,
-*followed by a new line
-*/
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char ch;
-for(ch = 'a'; ch<= 'z', ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
