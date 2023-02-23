388efd7cffeb7e2fc2d9db9774ebab83240d4c6f
sweetpotatoX <abdobahaa53@gmail.com>
2023-02-22 22:56:03 -0800

    gg

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,10 +10,14 @@
 int main(void)
 {
 int i;
+
 for (i = 1; i <= 100; i++)
 {
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
 }
-else if (i % 5 == 0 && i % 3 != 0)printf(" Fizz");
+else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
 }
@@ -31,5 +35,6 @@ printf(" %d", i);
 }
 }
 printf("\n");
+
 return (0);
 }
