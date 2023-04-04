bb59be01c870871d137af3be15220d921e83ae07
sweetpotatoX <abdobahaa53@gmail.com>
2023-04-04 08:22:17 -0700

    all mandatory

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,41 +1,46 @@
 #include "lists.h"
 
 /**
- * delete_node_at_index - deletes the node at a specific index of a linked list.
- * @head: pointer to a pointer to the head of the list.
- * @index: the index of the node to be deleted.
+ * delete_nodeint_at_index - deletes the node at index
+ * of a linked list.
+ * @head: head of a list.
+ * @index: index of the list where the node is
+ * deleted.
  *
- * Return: 1 if the node was deleted successfully, -1 otherwise.
+ * Return: 1 if it succeeded, -1 if it failed.
  */
-int delete_node_at_index(list_t **head, unsigned int index)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int i;
-list_t *prev_node, *curr_node, *next_node;
-if (*head == NULL)
-return (-1);
-curr_node = *head;
+listint_t *prev;
+listint_t *next;
 
-if (index == 0)
+prev = *head;
+
+if (index != 0)
 {
-*head = curr_node->next;
-free(curr_node);
-return (1);
+for (i = 0; i < index - 1 && prev != NULL; i++)
+{
+prev = prev->next;
 }
-prev_node = curr_node;
-curr_node = curr_node->next;
-
-for (i = 1; curr_node != NULL && i < index; i++)
-{
-prev_node = curr_node;
-curr_node = curr_node->next;
 }
 
-if (curr_node == NULL)
+if (prev == NULL || (prev->next == NULL && index != 0))
+{
 return (-1);
+}
 
-next_node = curr_node->next;
-prev_node->next = next_node;
-free(curr_node);
+next = prev->next;
 
+if (index != 0)
+{
+prev->next = next->next;
+free(next);
+}
+else
+{
+free(prev);
+*head = next;
+}
 return (1);
 }
