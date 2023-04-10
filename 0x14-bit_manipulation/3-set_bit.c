5a46984b2e01128a4eee548cac5f2d61a56df142
sweetpotatoX <abdobahaa53@gmail.com>
2023-04-10 05:11:52 -0700

    lol

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,10 +5,10 @@
  *@num: number to change
  *Retuen: 1 if works otherwise -1
  */
-int set_bit(unsigned long int *j, unsigned int num)
-{
-if (index > (8 * sizeof(*j)) - 1)
+int set_bit(unsigned long int *n, unsigned int index);
+unsigned int j;
+if (index >= sizeof(unsigned long int) * 8)
 return (-1);
-*j = *j | (1 << num);
-retun (1);
+*n |= (1UL << index);
+return (1);
 }
