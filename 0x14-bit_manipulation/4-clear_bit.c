cc24f35945fc90e7bba23284fba22b92bb51e213
sweetpotatoX <abdobahaa53@gmail.com>
2023-04-10 06:09:53 -0700

    lol

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,20 +2,21 @@
 #iclude "main.h"
 
 /**
- *clear_bit - sets the value og the required bit to 0.
- *@n: Pointer to the number.
- *@index: the required to clear number.
+ *clear_bit - sets the value og the required bit to 0
+ *@n: Pointer to the number
+ *@index: the required to clear number
  *
- *Return: 1 if works, -1 if didn't.
+ *Return: 1 if works, -1 if didn't
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int j;
-unsigned int seet;
+unsigned int sweet;
+
 if (index > 64)
 return (-1);
-seet = index;
-for (j = 1; seet > 0; j *= 2, seet--)
+hold = sweet;
+for (j = 1; sweet > 0; j *= 2, sweet--)
 ;
 if ((*n >> index) & 1)
 *n -= j;
