1f444bc5761d0e522be2267edf504473be93e9d9
sweetpotatoX <67162997+sweetpotatoX@users.noreply.github.com>
2023-04-11 03:20:32 +0200

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,69 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-#include <stdio>
+
 /**
- *check_file_error - program checks if files will be opened.
- * @file_from: file from.
- * @file_to: file to.
- * @argv: arg vector.
- * Return: no return.
- */
-void check_file_error(int file_from, int file_to, char *argv[])
-{
-if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Cannot read from file %s\n", argv[1]);
-exit(98);
-}
-if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Cannot write to file %s\n", argv[2]);
-exit(99);
-}
-}
-/**
- * main - program check the code.
- * @argc: num of arguments.
- * @argv: arg vector.
- * Return: Always 0.
- */
+* main - program that copies the content of a file to another file
+* @argc: num argument
+* @argv: string argument
+* Return: 0
+*/
+
 int main(int argc, char *argv[])
 {
-int file_from, file_to, close_err;
-ssize_t n_chars, n_written;
-char buffer[1024];
+int file_from, file_to;
+int num1 = 1024, num2 = 0;
+char buf[1024];
+
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-exit(97);
-}
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 file_from = open(argv[1], O_RDONLY);
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-check_file_error(file_from, file_to, argv);
-n_chars = 1024;
-while (n_chars == 1024)
+if (file_from == -1)
 {
-n_chars = read(file_from, buffer, 1024);
-if (n_chars == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+| S_IRGRP | S_IWGRP | S_IROTH);
+if (file_to == -1)
 {
-check_file_error(-1, 0, argv);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(file_from), exit(99);
 }
-n_written = write(file_to, buffer, n_chars);
-if (n_written == -1)
+while (num1 == 1024)
 {
-check_file_error(0, -1, argv);
-}
-}
-close_err = close(file_from);
-if (close_err == -1)
+num1 = read(file_from, buf, 1024);
+if (num1 == -1)
 {
-dprintf(STDERR_FILENO, "Error: Cannot close file descriptor %d\n", file_from);
-exit(100);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
-close_err = close(file_to);
-if (close_err == -1)
-{
-dprintf(STDERR_FILENO, "Error: Cannot close file descriptor %d\n", file_to);
-exit(100);
+num2 = write(file_to, buf, num1);
+if (num2 < num1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
+
+if (close(file_from) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+if (close(file_to) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 return (0);
 }
